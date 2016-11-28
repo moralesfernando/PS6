6c114cb2afa475de94a49bbd80c267d93d19dcbd
moral <moral@128.4.98.140>
2016-11-27 19:45:19 -0500

    1st draft check over later

--- PS6_DAL/src/test/java/base/Person_Test.java
+++ PS6_DAL/src/test/java/base/Person_Test.java
@@ -6,6 +6,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.UUID;
 
@@ -20,26 +21,25 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 public class Person_Test {
-		
+
 	private static PersonDomainModel person1;
-	private static UUID person1UUID = UUID.randomUUID();			
-	
+	private static UUID person1UUID = UUID.randomUUID();
+
 	@BeforeClass
-	public static void personInstance() throws Exception{
-		
+	public static void personInstance() throws Exception {
+
 		Date person1Birth = null;
 
 		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-		
-		 person1 = new PersonDomainModel();
-		 
+
+		person1 = new PersonDomainModel();
+
 		try {
 			person1Birth = dateFormat.parse("1994-11-27");
 		} catch (ParseException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		person1.setPersonID(person1UUID);
 		person1.setFirstName("Mingkun");
 		person1.setMiddleName("a");
@@ -48,9 +48,99 @@ public class Person_Test {
 		person1.setCity("Elkton");
 		person1.setStreet("702 Stone Gate Blvd");
 		person1.setPostalCode(21921);
-		
 	}
-	
-	
 
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void AddPersonTest() {
+		PersonDomainModel per;
+		per = PersonDAL.getPerson(person1.getPersonID());
+		assertNull("Invalid person.", per);
+		PersonDAL.addPerson(person1);
+
+		per = PersonDAL.getPerson(person1.getPersonID());
+		System.out.println(person1.getPersonID() + "Added");
+		assertNotNull("Person added successfully.", per);
+
+		PersonDAL.deletePerson(person1.getPersonID());
+	}
+
+	@Test
+	public void UpdatePersonTest() {
+		PersonDomainModel per;
+		final String Lastname = "Smith";
+
+		per = PersonDAL.getPerson(person1.getPersonID());
+		assertNull("Invalid person.", per);
+		PersonDAL.addPerson(person1);
+
+		person1.setLastName(Lastname);
+		PersonDAL.updatePerson(person1);
+
+		per = PersonDAL.getPerson(person1.getPersonID());
+
+		assertTrue("No name change.", person1.getLastName() == Lastname);
+
+		PersonDAL.deletePerson(person1.getPersonID());
+	}
+
+	@Test
+	public void DeletePersonTest() {
+		PersonDomainModel per;
+		per = PersonDAL.getPerson(person1.getPersonID());
+		assertNull("Invalid Person.", per);
+
+		PersonDAL.addPerson(person1);
+		per = PersonDAL.getPerson(person1.getPersonID());
+		System.out.println(person1.getPersonID() + "Added");
+		assertNotNull("Person added successfully.", per);
+
+		PersonDAL.deletePerson(person1.getPersonID());
+		per = PersonDAL.getPerson(person1.getPersonID());
+		assertNull("Invalid person.", per);
+	}
+
+	@Test
+	public void GetPersonsTest() {
+		PersonDomainModel per;
+		per = PersonDAL.getPerson(person1.getPersonID());
+		assertNull("Invalid person.", per);
+
+		PersonDAL.addPerson(person1);
+		per = PersonDAL.getPerson(person1.getPersonID());
+		System.out.println(person1.getPersonID() + "Added");
+		assertNotNull("Person added successfully.", per);
+
+		ArrayList<PersonDomainModel> people = new ArrayList<PersonDomainModel>();
+		people = PersonDAL.getPersons();
+		System.out.println(people);
+		assertNotNull("Person retrieved successfully.", people);
+
+		PersonDAL.deletePerson(person1.getPersonID());
+	}
+
+	@Test
+	public void GetPersonTest() {
+		PersonDomainModel per;
+		PersonDAL.addPerson(person1);
+		per = PersonDAL.getPerson(person1.getPersonID());
+		System.out.println(person1.getPersonID() + "Added");
+
+		per = PersonDAL.getPerson(person1.getPersonID());
+		assertNotNull("Person added successfully.", per);
+
+		PersonDAL.deletePerson(person1.getPersonID());
+
+	}
 }
